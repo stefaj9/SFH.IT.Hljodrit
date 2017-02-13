252038ae5e3bd99637a6b95629ccd5d7f0ce4385
Arnar Leifsson <arnarleifs@github.com>
2017-02-13 20:33:34 +0000

    Changing folder structure, adding an Admin project to the solution

---
+++ SFH.IT.Hljodrit.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFH.IT.Hljodrit.Admin.MvcApplication" Language="C#" %>
---
+++ SFH.IT.Hljodrit.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SFH.IT.Hljodrit.Admin.Startup))]
+namespace SFH.IT.Hljodrit.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
