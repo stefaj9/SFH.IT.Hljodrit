4a1ea114b9379e3ed483c046875f6b37837d592e
Baldur Tryggvason <ballit85@gmail.com>
2017-02-17 11:30:44 +0000

    Add DbFactory, Repository and UnitOfWork to project

---
+++ SFH.IT.Hljodrit.Repositories/Base/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFH.IT.Hljodrit.Models;
+
+namespace SFH.IT.Hljodrit.Repositories.Base
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private HljodritEntities _dbContext;
+
+        public HljodritEntities Init()
+        {
+            return _dbContext ?? (_dbContext = new HljodritEntities());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (_dbContext != null)
+                _dbContext.Dispose();
+        }
+    }
+}
---
+++ SFH.IT.Hljodrit.Repositories/Base/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFH.IT.Hljodrit.Models;
+
+namespace SFH.IT.Hljodrit.Repositories.Base
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory _dbFactory;
+        private HljodritEntities _dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this._dbFactory = dbFactory;
+        }
+
+        public HljodritEntities DbContext
+        {
+            get { return _dbContext ?? (_dbContext = _dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
