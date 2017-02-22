ebcf644a2d22dde6c702232ae17d0a95779fa032
Arnar Leifsson <arnarleifs@github.com>
2017-02-22 16:09:24 +0000

    Creating a beautiful step-by-step modal

---
+++ SFH.IT.Hljodrit.Admin.Tests/Services/ProjectServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SFH.IT.Hljodrit.Admin.Tests.Services
+{
+    [TestClass]
+    public class ProjectServiceTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
--- SFH.IT.Hljodrit.Admin/App/components/modalSteps.js
+++ SFH.IT.Hljodrit.Admin/App/components/modalSteps.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default class ModalSteps extends React.Component {
     renderSteps() {
-        return this.props.steps.map((step) => {
+        return this.props.steps.map((step, idx) => {
+            let stepClass = '';
+            if (idx + 1 > this.props.currentStep) {
+                stepClass = 'step-unfinished';
+            } else if (idx + 1 < this.props.currentStep) {
+                stepClass = 'step-finished';
+            } else {
+                stepClass = 'step-current';
+            }
             return (
-                <div key={step.name} className="text-center step">
+                <div key={step.name} className={'text-center step ' + stepClass}>
                     <div className="icon"><i className={step.class + ' fa-2x'}></i></div>
                     <div className="step-title">{step.name}</div>
                 </div>
