aaa57f73846612c56e0e5c73e3dc545bd3e7656b
bjoggib <bjoggib@gmail.com>
2017-02-23 21:27:57 +0000

    get a list of instruments from api implemented

---
+++ SFH.IT.Hljodrit.Admin/Controllers/InstrumentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using SFH.IT.Hljodrit.Services.Interfaces;
+
+namespace SFH.IT.Hljodrit.Admin.Controllers
+{
+    [RoutePrefix("api/instruments")]
+    public class InstrumentController : ApiController
+    {
+        private readonly IInstrumentService _instrumentService;
+
+        public InstrumentController(IInstrumentService instrumentService)
+        {
+            _instrumentService = instrumentService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAllInstruments()
+        {
+            // TODO: Possibly add paging
+            return Ok(_instrumentService.GetAllInstruments());
+        }
+    }
+}
