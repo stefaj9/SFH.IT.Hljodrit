75890b21621742a68617d69b99d8d6b72e7aa372
Baldur Tryggvason <ballit85@gmail.com>
2017-03-05 18:11:28 +0000

    Add settingsController

---
+++ SFH.IT.Hljodrit.Admin/Controllers/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using SFH.IT.Hljodrit.Services.Interfaces;
+
+namespace SFH.IT.Hljodrit.Admin.Controllers
+{
+	[System.Web.Mvc.RoutePrefix("api")]
+	public class SettingsController : ApiController
+	{
+		private readonly ISettingsService _settingsService;
+
+		public SettingsController(ISettingsService settingsService)
+		{
+			_settingsService = settingsService;
+		}
+
+		[System.Web.Mvc.HttpGet]
+		[System.Web.Mvc.Route("exceptions")]
+		public IHttpActionResult GetAllExceptions([FromUri] int pageSize, [FromUri] int pageNumber)
+		{
+			return Ok(_settingsService.GetAllExceptions(pageSize, pageNumber));
+		}
+    }
+}
