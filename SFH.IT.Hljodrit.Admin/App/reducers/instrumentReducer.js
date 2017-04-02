edb24e4a29ac5d9484f2c71c961a4aa468632337
Arnar Leifsson <arnarleifs@github.com>
2017-04-02 22:18:23 +0000

    Fixing the z-index issue with the chips

--- SFH.IT.Hljodrit.Admin/App/reducers/instrumentReducer.js
+++ SFH.IT.Hljodrit.Admin/App/reducers/instrumentReducer.js
@@ -6,8 +6,13 @@ let initialState = {
 
 export default function(state = initialState, action) {
     switch (action.type) {
-        case 'GET_INSTRUMENT_SUGGESTIONS': 
-            let instrumentSuggestions = _.map(action.payload, 'instrumentNameIcelandic');
+        case 'GET_INSTRUMENT_SUGGESTIONS':
+            let instrumentSuggestions = _.map(action.payload, (i) => { 
+                return {
+                    idCode: i.idCode,
+                    instrumentNameIcelandic: i.instrumentNameIcelandic
+                }
+            });
             return Object.assign({}, state, {
                 instrumentSuggestions: instrumentSuggestions
             });
