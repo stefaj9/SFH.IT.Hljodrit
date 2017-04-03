73661737250c3dba9429797d14e97afb66e56a0e
Baldur Tryggvason <ballit85@gmail.com>
2017-04-03 12:18:52 +0000

    Commenting out an unused line in app.js for linting

--- SFH.IT.Hljodrit.Admin/App/components/common/app.js
+++ SFH.IT.Hljodrit.Admin/App/components/common/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from './header';
 import { getZipCodes, getCountries } from '../../actions/commonActions';
-import _ from 'lodash';
+//import _ from 'lodash';
 import '../handlers/songLengthHandler';
 
 class App extends React.Component {
