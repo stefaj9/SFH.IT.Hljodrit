3daeb868851e42eb5a6684b16a6ac483e2b448bd
Arnar Leifsson <arnarleifs@github.com>
2017-04-07 22:33:56 +0000

    Adding members to the group

--- SFH.IT.Hljodrit.Admin/App/components/project/performerGroup.js
+++ SFH.IT.Hljodrit.Admin/App/components/project/performerGroup.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import Chips from 'react-chips';
 import { connect } from 'react-redux';
+import { getPersonsByCriteria } from '../../actions/personActions';
+import { isFetchingList, hasStoppedFetchingList } from '../../actions/flowActions';
 import _ from 'lodash';
+import SelectPersonModal from './selectPersonModal';
+import { toastr } from 'react-redux-toastr';
 
 class PerformerGroup extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            group: [{
-                number: 1,
-                name: 'Arnar Leifsson',
-                role: '',
-                instruments: []
-            }, {
-                number: 2,
-                name: 'Björgvin Birkir Björgvinsson',
-                role: '',
-                instruments: []
-            }, {
-                number: 3,
-                name: 'Baldur Tryggvason',
-                role: '',
-                instruments: []
-            }]
+            isAddingPerson: false,
+            currentNumber: 0,
+            group: []
         }
+    }
+    addInstrument(instruments) {
+        //let group = _.cloneDeep(this.state.group);
+        console.log(instruments);
+    }
+    openPerformerSelectionModal(e) {
+        e.preventDefault();
+        this.setState({ isAddingPerson: true });
+    }
+    addPerformerToGroup(performer) {
+        performer.role = '';
+        performer.instruments = [];
+        let group = _.cloneDeep(this.state.group);
+        group = _.concat(group, performer);
+
+        toastr.success('Tókst!', 'Það tókst að bæta við flytjanda í hóp.');
+
+        this.setState({ group: group });
     }
     renderPerformerRoles() {
         return this.props.roles.map((role) => {
@@ -31,9 +40,6 @@ class PerformerGroup extends React.Component {
                 <option key={role.roleCode} value={role.roleCode}>{role.roleName}</option>
             );
         });
-    }
-    addInstrument(instruments, number) {
-        let group = _.cloneDeep(this.state.group);
     }
     renderGroup() {
         let suggestions = this.props.instrumentSuggestions;
@@ -43,22 +49,31 @@ class PerformerGroup extends React.Component {
             });
             suggestions = _.map(suggestions, 'instrumentNameIcelandic');
         }
-        return this.state.group.map((member) => {
+        if (this.state.group.length > 0) {
+            return this.state.group.map((member) => {
+                return (
+                    <div key={`${member.id}-${member.name}`} className="group text-center">
+                        <h4>{member.name}</h4>
+                        <label>Hlutverk</label>
+                        <select 
+                            name="group-member-role" 
+                            id={`group-member-role-${member.id}`} 
+                            className="form-control group-member-role">
+                            {this.renderPerformerRoles()}
+                        </select>
+                        <label>Hljóðfæri</label>
+                        <Chips
+                            value={member.instruments}
+                            onChange={(instruments) => this.addInstrument(instruments, member.id)}
+                            suggestions={suggestions} />
+                    </div>
+                );
+            });
+        } else {
             return (
-                <div key={`${member.number}-${member.name}`} className="group text-center">
-                    <h4>{member.name}</h4>
-                    <label>Hlutverk</label>
-                    <select name="group-member-role" id={`group-member-role-${member.number}`} className="form-control group-member-role">
-                        {this.renderPerformerRoles()}
-                    </select>
-                    <label>Hljóðfæri</label>
-                    <Chips
-                        value={member.instruments}
-                        onChange={(instruments) => this.addInstrument(instruments, member.number)}
-                        suggestions={suggestions} />
-                </div>
+                <h4 className="text-center">Hópurinn er tómur.</h4>
             );
-        });
+        }
     }
     renderSongSelection() {
         return this.props.songs.map((song) => {
@@ -83,8 +98,9 @@ class PerformerGroup extends React.Component {
                 <div className="row">
                     <div className="col-xs-12 text-right">
                         <button 
+                            onClick={(e) => this.openPerformerSelectionModal(e)}
                             className="btn btn-default">
-                            <i className="fa fa-plus fa-fw"></i> Bæta við í hóp
+                            Bæta við í hóp <i className="fa fa-plus fa-fw"></i>
                         </button>
                     </div>
                 </div>
@@ -103,6 +119,17 @@ class PerformerGroup extends React.Component {
                         <button className="btn btn-default">Færa yfir <i className="fa fa-arrow-right"></i></button>
                     </div>
                 </div>
+                <SelectPersonModal
+                    isOpen={this.state.isAddingPerson}
+                    close={() => this.setState({ isAddingPerson: false })}
+                    next={() => this.setState({ isAddingPerson: false })}
+                    registerPath="/api/persons"
+                    envelope={this.props.performersEnvelope}
+                    fetch={this.props.getPersonsByCriteria}
+                    beginFetch={this.props.isFetchingList}
+                    stoppedFetch={this.props.hasStoppedFetchingList}
+                    update={(performer) => this.addPerformerToGroup(performer)}
+                    steps={() => { return ( <h4>Velja flytjanda í hóp</h4> ) }} />
             </div>
         );
     }
@@ -115,8 +142,9 @@ PerformerGroup.propTypes = {
 function mapStateToProps(state) {
     return {
         instrumentSuggestions: state.instrument.instrumentSuggestions,
-        roles: state.person.personRoles
+        roles: state.person.personRoles,
+        performersEnvelope: state.person.personEnvelope
     };
 };
 
-export default connect(mapStateToProps, null)(PerformerGroup);
+export default connect(mapStateToProps, { isFetchingList, hasStoppedFetchingList, getPersonsByCriteria })(PerformerGroup);
