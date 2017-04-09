7f9cb442770229885491bf2c0883ecfab26a6e2d
Arnar Leifsson <arnarleifs@github.com>
2017-04-09 17:33:09 +0000

    Adding time-picker also to the song detail page.

--- SFH.IT.Hljodrit.Admin/App/components/songs/songDetails.js
+++ SFH.IT.Hljodrit.Admin/App/components/songs/songDetails.js
@@ -5,12 +5,17 @@ import Spinner from 'react-spinner';
 import _ from 'lodash';
 import moment from 'moment';
 import { DateField, DatePicker } from 'react-date-picker'
+import TimePicker from 'rc-time-picker';
 
 class SongDetails extends React.Component {
     componentWillReceiveProps(newProps) {
         if (_.keys(newProps.song).length > 0 && !this.state.hasFetched) {
+            let song = _.cloneDeep(newProps.song);
+            let durationSplit = newProps.song.duration.split(':');
+            song.duration = moment('2000-01-01').add(durationSplit[0], 'h').add(durationSplit[1], 'm').add(durationSplit[2], 's');
             this.setState({
-                currentSong: newProps.song
+                currentSong: song,
+                hasFetched: true
             });
         }
     }
@@ -23,7 +28,8 @@ class SongDetails extends React.Component {
 
         this.state = {
             currentSong: {},
-            hasFetched: false
+            hasFetched: false,
+            dirtyForm: false
         };
     }
     renderFormGroup(label, id, value, onChangeFunc) {
@@ -34,26 +40,63 @@ class SongDetails extends React.Component {
             </div>
         );
     }
+    renderSongLengthPicker() {
+        return (
+            <div className="form-group">
+                <label htmlFor="">Lengd lags</label>
+                <TimePicker
+                    showSecond={true}
+                    value={this.state.currentSong.duration}
+                    defaultValue={this.state.currentSong.duration}
+                    onChange={this.updateSongDuration.bind(this)} />
+            </div>
+        );
+    }
+    renderReleaseDatePicker() {
+        return (
+            <div className="form-group">
+                <label>Útgáfudagur lags</label>
+                <div>
+                    <DateField
+                        dateFormat="DD.MM.YYYY"
+                        forceValidDate={true}
+                        updateOnDateClick={true}
+                        defaultValue={moment(this.props.song.releaseDate).format('DD.MM.YYYY')}>
+                        <DatePicker
+                            navigation={true}
+                            locale="is"
+                            forceValidDate={true}
+                            highlightWeekends={true}
+                            highlightToday={true}
+                            weekNumbers={true}
+                            weekStartDay={1}
+                            onChange={this.updateSongReleaseDate.bind(this)} />
+                    </DateField>
+                </div>
+            </div>
+        );
+    }
     updateSongTitle(e) {
         let song = _.cloneDeep(this.state.currentSong);
         song.songTitle = e.target.value;
-        this.setState({ currentSong: song });
+        this.setState({ currentSong: song, dirtyForm: true });
     }
     updateSongIsrc(e) {
         let song = _.cloneDeep(this.state.currentSong);
         song.isrc = e.target.value;
-        this.setState({ currentSong: song });
+        this.setState({ currentSong: song, dirtyForm: true });
     }
-    updateSongDuration(e) {
-        let song = _.cloneDeep(this.state.currentSong);
-        song.duration = e.target.value;
-        this.setState({ currentSong: song });
+    updateSongDuration(value) {
+        if (moment(value).isValid()) {
+            let song = _.cloneDeep(this.state.currentSong);
+            song.duration = value;
+            this.setState({ currentSong: song, dirtyForm: true });
+        }
     }
-    updateSongReleaseDate(dateString, momentObj, config) {
+    updateSongReleaseDate(dateString, momentObj) {
         let song = _.cloneDeep(this.state.currentSong);
-        console.log(momentObj.dateMoment._d);
         song.releaseDate = momentObj.dateMoment._d;
-        this.setState({ currentSong: song });
+        this.setState({ currentSong: song, dirtyForm: true });
     }
     renderSongInfo() {
         if (!this.props.isFetching) {
@@ -66,30 +109,11 @@ class SongDetails extends React.Component {
                             {this.renderFormGroup('ISRC kóði', 'song-isrc', this.state.currentSong.isrc, this.updateSongIsrc)}
                         </div>
                         <div className="col-xs-12 col-sm-6">
-                            {this.renderFormGroup('Lengd lags', 'song-duration', this.state.currentSong.duration, this.updateSongDuration)}
-                            <div className="form-group">
-                                <label>Útgáfudagur lags</label>
-                                <div>
-                                    <DateField
-                                        dateFormat="DD.MM.YYYY"
-                                        forceValidDate={true}
-                                        updateOnDateClick={true}
-                                        defaultValue={moment(this.props.song.releaseDate).format('DD.MM.YYYY')}>
-                                        <DatePicker
-                                            navigation={true}
-                                            locale="is"
-                                            forceValidDate={true}
-                                            highlightWeekends={true}
-                                            highlightToday={true}
-                                            weekNumbers={true}
-                                            weekStartDay={1}
-                                            onChange={this.updateSongReleaseDate.bind(this)} />
-                                    </DateField>
-                                </div>
-                            </div>
+                            {this.renderSongLengthPicker()}
+                            {this.renderReleaseDatePicker()}
                         </div>
                         <div className="col-xs-12 text-right">
-                            <button className="btn btn-default btn-primary">Vista</button>
+                            <button disabled={!this.state.dirtyForm} className="btn btn-default btn-primary">Vista</button>
                         </div>
                     </div>
                 </div>
