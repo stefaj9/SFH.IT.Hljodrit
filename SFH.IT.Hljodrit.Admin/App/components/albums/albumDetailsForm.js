cfc256338cacf51e2cec309d182d4263c5f3d8b8
bjoggib <bjoggib@gmail.com>
2017-04-10 15:57:38 +0000

    more refactoring

--- SFH.IT.Hljodrit.Admin/App/components/albums/albumDetailsForm.js
+++ SFH.IT.Hljodrit.Admin/App/components/albums/albumDetailsForm.js
@@ -6,7 +6,7 @@ import { getPublishersByCriteria, getLabelsByPublisherId } from '../../actions/o
 import SelectPersonModal from '../project/selectPersonModal';
 import _ from 'lodash';
 import Spinner from 'react-spinner';
-
+import AlbumDetailsFormbla from './AlbumDetailsFormbla';
 class AlbumDetailsForm extends React.Component {
 
     componentWillReceiveProps(newProps) {
@@ -34,7 +34,6 @@ class AlbumDetailsForm extends React.Component {
             selectedAlbum: {
                 mainArtistName: '',
                 publisher: '',
-                publisherId: -1,
                 countryOfPublication: '',
                 countryOfProduction: '',
                 label: '',
@@ -51,6 +50,13 @@ class AlbumDetailsForm extends React.Component {
     }
 
     populateLabelOptions() {
+        if (this.props.selectedOrganizationLabels.length === 0) {
+            return (
+                <option key={-1}
+                    value="Ekki skráð"> Ekki skráð
+                </option>
+            );
+        }
         return this.props.selectedOrganizationLabels.map((label, idx) => {
             return (
                 <option key={idx}
@@ -107,103 +113,35 @@ class AlbumDetailsForm extends React.Component {
     }
 
     validateAlbum(album) {
-        let invalid = 'ekki skráð'
+        let invalid = 'Ekki skráð'
         for (var key in album) {
             if (album[key] === null || album[key] === undefined) {
                 album[key] = invalid;
+
             }
         }
-        album.publisherId = invalid ? -1 : album.publisherId;
+        album.publisherId = (album.publisherId === invalid) ? -1 : album.publisherId;
     }
 
     renderForm() {
         if (this.state.hasFetched) {
             return (
-                <form>
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Plötuheiti</label>
-                            <input type="text" className="form-control"
-                                value={this.state.selectedAlbum.albumTitle}
-                                onChange={(e) => this.updateAlbumField('albumTitle', e.target.value)}/>
-                        </div>
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Aðalflytjandi</label>
-                            <div className="input-group">
-                                <input type="text" className="form-control"disabled="true"
-                                    value={this.state.selectedAlbum.mainArtistName}
-                                    />
-                                    <div className="input-group-btn">
-                                        <button type="button" className="btn btn-primary"
-                                            onClick={() => this.openModal(this.props.getMainArtistsByCriteria,
-                                                                this.props.mainArtistEnvelope, 'Breyta aðalflytjanda',
-                                                                this.updateMainArtist.bind(this))}>
-                                            Breyta
-                                        </button>
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Útgefandi</label>
-                            <div className="input-group">
-                                <input type="text" className="form-control" disabled="true"
-                                    value={this.state.selectedAlbum.publisher}/>
-                                <div className="input-group-btn">
-                                    <button type="button" className="btn btn-primary"
-                                        onClick={() => this.openModal(this.props.getPublishersByCriteria,
-                                                            this.props.organizationEnvelope,'Breyta útgefanda',
-                                                            this.updatePublisher.bind(this))}>
-                                        Breyta
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Label</label>
-                                <select className="form-control"
-                                    onChange={(e) => this.updateAlbumField('label', e.target.value)}
-                                    value={this.state.selectedAlbum.label}>
-                                    {this.populateLabelOptions()}
-                                </select>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Framleiðsluland</label>
-                            <select className="form-control"
-                                onChange={(e) => this.updateAlbumField('countryOfProduction', e.target.value)}
-                                value={this.state.selectedAlbum.countryOfProduction}>
-                                {this.props.countryOptions()}
-                            </select>
-                        </div>
-                        <div className="col-xs-12 col-sm-6 form-group">
-                            <label>Útgáfuland</label>
-                            <select className="form-control"
-                                onChange={(e) => this.updateAlbumField('countryOfPublication', e.target.value)}
-                                value={this.state.selectedAlbum.countryOfPublication}>
-                                {this.props.countryOptions()}
-                            </select>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-default btn-primary pull-right"
-                        disabled={!this.state.selectedAlbumHasChanged}
-                        onClick={(e) => this.updateSelectedAlbum(e)}>
-                        Vista
-                    </button>
-                    <SelectPersonModal
-                        isOpen={this.state.isModalOpen}
-                        fetch={this.state.currentFetchMethod}
-                        beginFetch={this.props.isFetchingList}
-                        stoppedFetch={this.props.hasStoppedFetchingList}
-                        next={() => this.setState({isModalOpen: false})}
-                        close={() => this.setState({isModalOpen: false})}
-                        envelope={this.state.envelope}
-                        update={(newElement) => this.state.currentUpdateFunction(newElement)}
-                        steps={() => { return ( <h4>{ this.state.typeOfAction }</h4> ) } }
-                     />
-                </form>
+                <AlbumDetailsFormbla
+                    hasFetched={ this.state.hasFetched }
+                    album={ this.state.selectedAlbum }
+                    updateAlbumField={ this.updateAlbumField.bind(this) }
+                    openModal={ this.openModal.bind(this) }
+                    getMainArtistsByCriteria={ this.props.getMainArtistsByCriteria.bind(this) }
+                    mainArtistEnvelope={ this.props.mainArtistEnvelope }
+                    updateMainArtist={ this.updateMainArtist.bind(this) }
+                    getPublishersByCriteria={ this.props.getPublishersByCriteria.bind(this) }
+                    organizationEnvelope={ this.props.organizationEnvelope }
+                    updatePublisher={ this.updatePublisher.bind(this) }
+                    populateLabelOptions={ this.populateLabelOptions.bind(this) }
+                    countryOptions={ this.props.countryOptions.bind(this) }
+                    updateSelectedAlbum={ this.updateSelectedAlbum.bind(this) }
+                    selectedAlbumHasChanged={ this.state.selectedAlbumHasChanged }
+                />
             );
         }
     }
