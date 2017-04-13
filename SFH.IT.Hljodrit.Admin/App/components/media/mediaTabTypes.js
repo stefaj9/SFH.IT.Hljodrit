9cba3f34a6bec9e4a16afa7a70fece08406f1034
Baldur Tryggvason <ballit85@gmail.com>
2017-04-13 16:39:40 +0000

    Removed no longer relevant files

--- SFH.IT.Hljodrit.Admin/App/components/media/mediaTabTypes.js
+++
@@ -1,5 +0,0 @@
-export const MEDIA_RECORDING = 'Hljóðrit';
-export const PARTY_REAL ='Aðilar';
-export const MEDIA_PRODUCT_PACKAGE ='Plötur';
-export const PARTY_MAINARTIST ='Aðalflytjendur';
-export const ORGANIZATION ='Framleiðendur';
