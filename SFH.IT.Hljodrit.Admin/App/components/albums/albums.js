751583945f5aee9a2d2632ac9d0226bf42c97217
Arnar Leifsson <arnarleifs@github.com>
2017-04-25 15:53:57 +0000

    Adding create album link in /albums

--- SFH.IT.Hljodrit.Admin/App/components/albums/albums.js
+++ SFH.IT.Hljodrit.Admin/App/components/albums/albums.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router';
 import {getAllAlbums} from '../../actions/albumsActions';
 import PageSelector from '../common/pageSelector';
 import Paging from '../common/paging';
@@ -56,6 +57,9 @@ class Albums extends React.Component {
         return (
             <div>
                 <h2>Plötur</h2>
+                <div className="add-project space-20 text-right">
+                    <Link to='albums/createalbum'><i className="fa fa-2x fa-plus"></i></Link>
+                </div>
                 <div className="row space-20">
                     <div className="col-xs-12 col-sm-8 no-padding">
                         <SearchBar 
