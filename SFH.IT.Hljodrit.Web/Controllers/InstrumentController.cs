02d8a094a894c4777686a555df3255f307a2fba1
Arnar Leifsson <arnarleifs@github.com>
2017-04-27 14:22:28 +0000

    Adding now InstrumentController to fetch the instrumentSuggestions.

---
+++ SFH.IT.Hljodrit.Web/Controllers/InstrumentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using SFH.IT.Hljodrit.Services.Interfaces;
+
+namespace SFH.IT.Hljodrit.Web.Controllers
+{
+    [RoutePrefix("api/instruments")]
+    public class InstrumentController : ApiController
+    {
+        private readonly IInstrumentService _instrumentService;
+
+        public InstrumentController(IInstrumentService instrumentService)
+        {
+            _instrumentService = instrumentService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAllInstruments()
+        {
+            return Ok(_instrumentService.GetAllInstruments());
+        }
+    }
+}
