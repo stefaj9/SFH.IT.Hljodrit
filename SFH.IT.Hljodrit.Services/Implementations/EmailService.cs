c95cb4652331db5074a7651a7aef8bcd4bbb78cc
Arnar Leifsson <arnarleifs@github.com>
2017-05-03 22:34:04 +0000

    Changing the MX settings for the EmailService.cs

--- SFH.IT.Hljodrit.Services/Implementations/EmailService.cs
+++ SFH.IT.Hljodrit.Services/Implementations/EmailService.cs
@@ -5,10 +5,16 @@ namespace SFH.IT.Hljodrit.Services.Implementations
 {
     public static class EmailService
     {
-        private const string Smtp = "mx10.vist.is";
+        private const string Smtp = "mx1.basis.is";
         public static Task SendAsync(string to, string from, string subject, string message)
         {
-            SmtpClient client = new SmtpClient {Host = "smtp.ru.is"};
+            SmtpClient client = new SmtpClient
+            {
+                Host = Smtp,
+                UseDefaultCredentials = true,
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
             MailMessage msg = new MailMessage(from, to)
             {
                 IsBodyHtml = true,
