5a35f1e653b02b2312a2447281fdf3bb32ca644c
Baldur Tryggvason <ballit85@gmail.com>
2017-05-11 19:40:29 +0000

    More comments in Dto objects

--- SFH.IT.Hljodrit.Common/Dto/TrackDto.cs
+++ SFH.IT.Hljodrit.Common/Dto/TrackDto.cs
@@ -4,22 +4,46 @@ using Newtonsoft.Json;
 
 namespace SFH.IT.Hljodrit.Common.Dto
 {
+    /// <summary>
+    /// An object that contains a single track.
+    /// </summary>
     public class TrackDto
     {
+        /// <summary>
+        /// The id of the track.
+        /// </summary>
         [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
+
+        /// <summary>
+        /// The id of the project that the track appears on.
+        /// </summary>
         [Required]
         [JsonProperty(PropertyName = "projectId")]
         public int ProjectId { get; set; }
+
+        /// <summary>
+        /// The name of the track.
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         [JsonProperty(PropertyName = "trackName")]
         public string TrackName { get; set; }
+
+        /// <summary>
+        /// The isrc code for the track.
+        /// </summary>
         [JsonProperty(PropertyName = "isrc")]
         public string Isrc { get; set; }
+
+        /// <summary>
+        /// The duration of the track.
+        /// </summary>
         [JsonProperty(PropertyName = "duration")]
         public TimeSpan? Duration { get; set; }
+
         [JsonProperty(PropertyName = "doNotPublish")]
         public bool DoNotPublish { get; set; }
+
         [Required]
         [JsonProperty(PropertyName = "trackOrder")]
         public int TrackOrder { get; set; }
--- SFH.IT.Hljodrit.Common/Dto/ZipCodeDto.cs
+++ SFH.IT.Hljodrit.Common/Dto/ZipCodeDto.cs
@@ -2,10 +2,20 @@
 
 namespace SFH.IT.Hljodrit.Common.Dto
 {
+    /// <summary>
+    /// Contains a single zip code object.
+    /// </summary>
     public class ZipCodeDto
     {
+        /// <summary>
+        /// The literal zip code f.e. 101
+        /// </summary>
         [JsonProperty(PropertyName = "code")]
         public string Code { get; set; }
+
+        /// <summary>
+        /// The area for the code f.e. Reykjavik
+        /// </summary>
         [JsonProperty(PropertyName = "area")]
         public string Area { get; set; }
     }
